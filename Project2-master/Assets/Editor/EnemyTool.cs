5cd873c0bd3648cb7c8012adf416bdab9b7ea31f
svc-student <svc-student@student.directory.jccc>
2017-02-22 13:23:29 -0600

    Fixing Errors

--- Project2-master/Assets/Editor/EnemyTool.cs
+++ Project2-master/Assets/Editor/EnemyTool.cs
@@ -7,8 +7,9 @@ using UnityEditor.Sprites;
 public class EnemyTool : EditorWindow {
 
 	public List<Enemies> enemyList = new List<Enemies>();
-	string nameString;
+	string nameString ="";
 	bool nameFlag = false;
+	bool spriteFlag = false;
 	string[] enemyNames;
 
 	int currentChoice = 0;
@@ -63,6 +64,10 @@ public class EnemyTool : EditorWindow {
 
 		EditorGUILayout.LabelField("Attack Speed");
 		attackTime = EditorGUILayout.FloatField (attackTime);
+		if (attackTime > 20f || attackTime < 1f) 
+		{
+			attackTime = 1f;
+		}
 
 		magicUser = EditorGUILayout.BeginToggleGroup ("Magic User", magicUser);
 		EditorGUILayout.LabelField("Mana Pool");
@@ -79,9 +84,15 @@ public class EnemyTool : EditorWindow {
 		if (nameFlag) 
 		{
 			EditorGUILayout.HelpBox ("Name can not be blank", MessageType.Error);
+		} 
+		if (spriteFlag) 
+		{
+			EditorGUILayout.HelpBox ("Sprite cannot be empty", MessageType.Error);
 		}
+
 		if (currentChoice == 0) {
-			if (GUILayout.Button ("Create")) {
+			if (GUILayout.Button ("Create")) 
+			{
 				creatEnemy ();
 			}
 		} 
@@ -102,6 +113,8 @@ public class EnemyTool : EditorWindow {
 				attack = 1;
 				defense = 1;
 				agility = 1;
+				attackTime = 1;
+				enemySprite = null;
 			}
 			else 
 			{
@@ -145,32 +158,52 @@ public class EnemyTool : EditorWindow {
 
 	private void creatEnemy()
 	{
-		if (nameString == "") 
+		if (nameString == "" || nameString ==null) 
 		{
 			nameFlag = true;
-		} 
+			return;
+		}
+
+
+		if (enemySprite == null) 
+		{
+			spriteFlag = true;
+			return;
+		}
 		else 
 		{
 			Enemies myEnemy = ScriptableObject.CreateInstance<Enemies>();
 			myEnemy.emname = nameString;
+			myEnemy.atk = attack;
+			myEnemy.health = health;
+			myEnemy.def = defense;
+			myEnemy.agi = agility;
+			myEnemy.atkTime = attackTime;
+			myEnemy.isMagic = magicUser;
+			myEnemy.manaPool = mana;
+			myEnemy.mySprite = enemySprite;
 			AssetDatabase.CreateAsset (myEnemy, "Assets/Resources/Data/EnemyData/"+myEnemy.emname.Replace(" ", "_")+".asset");
 			nameFlag = false;
+			spriteFlag = false;
 			GetEnemies ();
 		}
 	}
 
 	private void alterEnemy()
 	{
-		enemyList [0].health = health;
-		enemyList [0].atk = attack;
-		enemyList [0].def = defense;
-		enemyList [0].agi = agility;
-		enemyList [0].atkTime = attackTime;
+		enemyList [currentChoice - 1].emname = nameString;
+		enemyList [currentChoice - 1].health = health;
+		enemyList [currentChoice - 1].atk = attack;
+		enemyList [currentChoice - 1].def = defense;
+		enemyList [currentChoice - 1].agi = agility;
+		enemyList [currentChoice - 1].atkTime = attackTime;
 
-		enemyList [0].isMagic = magicUser;
-		enemyList [0].manaPool = mana;
+		enemyList [currentChoice - 1].isMagic = magicUser;
+		enemyList [currentChoice - 1].manaPool = mana;
 
-		enemyList [0].mySprite = enemySprite;
+		enemyList [currentChoice - 1].mySprite = enemySprite;
+
+		AssetDatabase.SaveAssets ();
 	}
 	
 }
